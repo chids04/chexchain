81036c30db8fefe6baa891262583abf8726a8b2f
chids <guyepic39@gmail.com>
2025-02-18 12:44:48 +0000

    adding gitignore

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_appblockchain_FILE /Users/chids/Documents/uni/cs3bc/blockchain/build/appblockchain.app/Contents/MacOS/appblockchain)
-set(__QT_DEPLOY_TARGET_appblockchain_TYPE EXECUTABLE)
