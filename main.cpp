9c5f3786c6817104e941a51a5a93c455e4f33b86
chids04 <guyepic39@gmail.com>
2025-02-18 17:05:27 +0000

    cleaning up main file

--- main.cpp
+++ main.cpp
@@ -19,15 +19,6 @@ int main(int argc, char *argv[])
         Qt::QueuedConnection);
     engine.loadFromModule("blockchain", "Main");
 
-    BlockchainAssignment::Wallet::Wallet myWallet;
-    std::cout << "Private Key: " << myWallet.privateKey << "\n";
-
-    std::string hash = "0000abc1e11b8d37c1e1232a2ea6d290cddb0c678058c37aa766f813cbbb366e";
-    std::string signature = BlockchainAssignment::Wallet::Wallet::CreateSignature(myWallet.publicID, myWallet.privateKey, hash);
-
-    bool isValid = BlockchainAssignment::Wallet::Wallet::ValidateSignature(myWallet.publicID, hash, signature);
-    std::cout << "Signature Valid: " << (isValid ? "YES" : "NO") << "\n";
-
     auto b_app = engine.singletonInstance<BlockchainApp*>("blockchain", "BlockchainApp");
     b_app->init();
 
